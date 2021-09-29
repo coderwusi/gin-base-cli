7621276ff961f3b30bf997a107f9e68e430e8808
吴杰克 <jieke.wu@zhenai.com>
2021-09-29 20:25:39 +0800

    引入viper、gorm，建立mysql数据库连接

---
+++ model/init.go
@@ -0,0 +1,37 @@
+package model
+
+import (
+	"fmt"
+	"gin-base-cli/setting"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+// DB 数据库链接单例
+
+var DB *gorm.DB
+
+func MysqlInit(config *setting.MySQLConfig) {
+	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?%s", config.Username, config.Password, config.Host, config.Port, config.Dbname, config.Suffix)
+	DB, err = gorm.Open(mysql.New(mysql.Config{
+		DSN:                       dsn,   // DSN data source name
+		DefaultStringSize:         256,   // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
+	}), &gorm.Config{NamingStrategy: schema.NamingStrategy{SingularTable: true}})
+	if err != nil {
+		fmt.Println("数据库连接错误：", err)
+		return
+	}
+
+	if err := DB.AutoMigrate(); err != nil {
+		fmt.Println("数据库迁移错误：", err)
+		return
+	}
+
+	fmt.Println("success")
+}
---
+++ setting/init.go
@@ -0,0 +1,42 @@
+package setting
+
+import (
+	"fmt"
+	"github.com/spf13/viper" //第三方配置管理神器
+)
+
+var Conf = new(TotalConfig)
+
+type TotalConfig struct {
+	*MySQLConfig `mapstructure:"mysql"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	Username     string `mapstructure:"username"`
+	Password     string `mapstructure:"password"`
+	Dbname       string `mapstructure:"dbname"`
+	Suffix       string `mapstructure:"suffix"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+func Init() {
+
+	viper.SetConfigFile("config.yaml") //读取配置
+	//viper.AddConfigPath("./")
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("viper.ReadInConfig failed, err:%v\n", err)
+		return
+	} //捕捉viper读取错误
+
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("viper.Unmarshal failed, err:%v\n", err)
+		return
+	} //捕捉viper反序列化错误
+
+	fmt.Println(Conf.MySQLConfig)
+
+}
